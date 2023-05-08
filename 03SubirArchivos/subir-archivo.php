14f5ccce422b225a9a506424f63c867461815570
Smogg21 <spazzaglia98@gmail.com>
2023-05-08 12:46:15 -0500

    m,

---
+++ 03SubirArchivos/subir-archivo.php
@@ -0,0 +1,20 @@
+<?php
+    foreach($_FILES["archivo_fls"] as $clave => $valor){
+        echo"Propiedad: $clave --- Valor: $valor <br>";
+    }
+
+
+
+    $archivo = $_FILES["archivo_fls"]["tmp_name"];
+    $destino = "archivos/".$_FILES["archivo_fls"]["name"];
+
+    if($_FILES["archivo_fls"]["type"] == "text/plain"){
+        move_uploaded_file($archivo, $destino);
+        echo"Archivo Subido";
+    
+    } else{
+        echo"Solo se admiten archivos de texto plano <br> <a href='enviar-archivo.php'>Regresar</a>";
+    }
+
+
+?>
