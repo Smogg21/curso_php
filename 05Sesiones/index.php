076c40b34896bb1612b4187c3029a7a530863ad0
Smogg21 <spazzaglia98@gmail.com>
2023-05-09 18:41:57 -0500

    Avanzando php básico 7 min 16:43

---
+++ 05Sesiones/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sesiones con PHP</title>
+</head>
+<body>
+    
+    <form action="control.php" name="autentificacion_frm" method="post" enctype="application/x-www-form-urlencoded">
+        <?php
+        error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+        if ($_GET["error"]=="si") {
+            echo "<span style='color:red;'>Verifica tus datos</span>";
+        }else {
+            echo "<span>Introduce tus datos</span>";
+            }
+        ?>
+        <br><br>
+        Usuario: <input type="text" name="usuario_txt"/> <br><br>
+        Password: <input type="password" name="password_txt"/> <br><br>
+        <input type="submit" value="Entrar" name="entrar_btn"/>
+    </form>
+</body>
+</html>
